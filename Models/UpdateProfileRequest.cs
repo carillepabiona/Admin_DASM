2e7224d5f8e6d022bb17cd8c2cf291b35bc8d986
Carille Pabiona <carillepabiona49@gmail.com>
2026-05-11 00:34:57 +0800

    CHANGE PASS DONE!

---
+++ Models/UpdateProfileRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Admin_DASM.Models
+{
+    public class UpdateProfileRequest
+    {
+        public string ContactNumber { get; set; } = "";
+
+        public string Address { get; set; } = "";
+    }
+}
