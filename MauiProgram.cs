274932bd99d7623bc8db49c684d00a8483ff00ae
Carille Pabiona <carillepabiona49@gmail.com>
2026-05-12 20:24:58 +0800

    DONE ACTIONS

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -27,7 +27,7 @@ namespace Admin_DASM
                 new HttpClient
                 {
                     BaseAddress =
-                        new Uri("http://192.168.254.102:5043/")
+                        new Uri("https://localhost:7209/")
                 });
 
             // SERVICES
